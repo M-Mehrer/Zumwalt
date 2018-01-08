5e9f25b7e11f465faa2438bff37ea449ecb984c6
ModuloGames <max.granzow@stud.hs-mannheim.de>
2018-01-08 11:08:35 +0100

    Basic events

--- server/server.js
+++ server/server.js
@@ -8,10 +8,47 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const highscores = require('./api/highscores');
 
+let ships = [];
+let players = [];
+let names = [];
 
-io.on('connection', function(socket){ // eslint-disable-line no-unused-vars 
+
+io.on('connection', function(socket){ // eslint-disable-line no-unused-vars
+	players.push(socket);
+	//ships.push([]);
+	//names.push([]);
+
+	let i = players.length - 1;
+	let enemy = (i % 2) === 0 ? i + 1 : i - 1;
+
+	socket.on('ships', (shipData) => {
+		console.log(i + ": Ships sent.");
+		// TODO: Check ships
+		ships[i] = shipData.ships;
+
+		if(ships[enemy] != undefined) {
+			// Starten
+			let rand = (Math.floor(Math.random() * 2))?true:false;
+			players[i].emit('beginner', rand);
+			players[enemy].emit('beginner', !rand);
+		}
+	});
+
+	socket.on('turn', (turnData) => {
+		// TODO
+
+	});
+	
+	socket.on('disconnect', (reason) => {
+		players[enemy].emit('error', 'Enemy disconnected.');
+	})
+
+
 	console.log('A user connected.');
 });
+
+
+
 
 
 app.use(function(req, res, next) {
